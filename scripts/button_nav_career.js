fe812043303cc6a5c4da58d195eaa1d2289ba4ac
Devientt <toosmrt1@gmail.com>
2024-11-03 21:30:22 +0400

    yes

--- scripts/button_nav_career.js
+++ scripts/button_nav_career.js
@@ -2,48 +2,59 @@
     This script handles the content assignment of career data
 */
 
-const CareerNames = 
-[
+const CareerNames = [
     "education",
-    "work"
-]
+    "work",
+    "weapons" // Ajout de la nouvelle catégorie
+];
 
 let CareerText = [];
-
 let careerTextArea = document.getElementById("project_data_area");
 
 /* Adds the included text data into the array of text. */
-for(let i = 0; i < CareerNames.length; ++i)
-{
+for (let i = 0; i < CareerNames.length; ++i) {
     CareerText.push("");
     fetch("./Data/pages/careers/" + CareerNames[i] + ".html")
-    .then( r => r.text() )
-    .then( t => CareerText[i] = t )
-
+        .then(r => r.text())
+        .then(t => CareerText[i] = t);
 }
 
-function LoadCareer(button, btnIndex)
-{
-    if(button.getAttribute("class") == "btn-career-5-active") /* case where you select the active button */
-    {
-        var x = document.getElementsByClassName("btn-career-5-active");
+function LoadCareer(button, btnIndex) {
+    // Supprimer les classes d'arrière-plan spécifiques existantes
+    careerTextArea.classList.remove("weapons-background");
 
-        x[0].className = "btn-career-5";
+    if (button.getAttribute("class") == "btn-career-5-active") {
+        // Désactiver l'onglet actif s'il est re-sélectionné
+        let activeButton = document.getElementsByClassName("btn-career-5-active");
+
+        activeButton[0].className = "btn-career-5";
         careerTextArea.innerHTML = "";
         careerTextArea.style.opacity = 0;
-    }
-    else 
-    {
-        var x = document.getElementsByClassName("btn-career-5-active");
-        if(x.length != 0) /* case where another project is currently active and being displayed. */
-        {
-            x[0].className = "btn-career-5";
+    } else {
+        // Désactiver l'onglet précédemment actif
+        let activeButton = document.getElementsByClassName("btn-career-5-active");
+        if (activeButton.length !== 0) {
+            activeButton[0].className = "btn-career-5";
             careerTextArea.innerHTML = "";
             careerTextArea.style.opacity = 0;
         }
 
-        /* Set this button as the active display */
-        careerTextArea.innerHTML = CareerText[btnIndex];
+        // Charger le contenu et appliquer les styles en fonction de l'index
+        if (btnIndex === 2) { // Si la catégorie est "Weapons"
+            careerTextArea.classList.add("weapons-background");
+            careerTextArea.innerHTML = `
+                <h2>Weapons</h2>
+                <p>This section contains information about weapons.</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/aLND1_Rx4BQ" 
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen></iframe>
+            `;
+        } else {
+            careerTextArea.innerHTML = CareerText[btnIndex];
+        }
+
+        // Appliquer les styles actifs et afficher le contenu
         careerTextArea.style.opacity = 1;
         document.getElementById(button.getAttribute("id")).className = "btn-career-5-active";
     }
